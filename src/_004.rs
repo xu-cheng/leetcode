103e70c799747aca0b9b11f019142fc52c62de7c
Xu Cheng <git@xuc.me>
2018-12-22 00:40:50 +0800

    add 004

---
+++ src/_004.rs
@@ -0,0 +1,66 @@
+use std::cmp;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        let (v1, v2) = if nums1.len() > nums2.len() {
+            (&nums2, &nums1)
+        } else {
+            (&nums1, &nums2)
+        };
+        let mut imin = 0;
+        let mut imax = v1.len();
+        let half_len = (v1.len() + v2.len() + 1) / 2;
+        while imin <= imax {
+            let i = (imin + imax) / 2;
+            let j = half_len - i;
+            if i < v1.len() && v1[i] < v2[j - 1] {
+                imin = i + 1;
+            } else if i > 0 && v1[i - 1] > v2[j] {
+                imax = i - 1;
+            } else {
+                let lhs_max = if i == 0 {
+                    v2[j - 1]
+                } else if j == 0 {
+                    v1[i - 1]
+                } else {
+                    cmp::max(v1[i - 1], v2[j - 1])
+                };
+
+                if (v1.len() + v2.len()) % 2 == 1 {
+                    return f64::from(lhs_max);
+                }
+
+                let rhs_min = if i == v1.len() {
+                    v2[j]
+                } else if j == v2.len() {
+                    v1[i]
+                } else {
+                    cmp::min(v1[i], v2[j])
+                };
+
+                return f64::from(lhs_max + rhs_min) / 2.0;
+            }
+        }
+
+        unreachable!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_median_sorted_arrays() {
+        assert_eq!(
+            Solution::find_median_sorted_arrays(vec![1, 3], vec![2]),
+            2.0
+        );
+        assert_eq!(
+            Solution::find_median_sorted_arrays(vec![1, 2], vec![3, 4]),
+            2.5
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod _001; // Two Sum
 pub mod _002; // Add Two Numbers
 pub mod _003; // Longest Substring Without Repeating Characters
+pub mod _004; // Median of Two Sorted Arrays
