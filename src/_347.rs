9a3725f1f0440945f59e582260adbd4b37ed13b1
Xu Cheng <git@xuc.me>
2019-01-03 21:19:55 +0800

    add 347

---
+++ src/_347.rs
@@ -0,0 +1,36 @@
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut freqs: HashMap<i32, usize> = HashMap::new();
+        for n in &nums {
+            *freqs.entry(*n).or_insert(0) += 1;
+        }
+        let mut heap = BinaryHeap::with_capacity(freqs.len());
+        for (n, freq) in freqs.iter() {
+            heap.push((*freq, *n));
+        }
+        let mut ans: Vec<i32> = Vec::with_capacity(k as usize);
+        for _ in 0..k {
+            ans.push(heap.pop().unwrap().1);
+        }
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solution() {
+        assert_eq!(
+            Solution::top_k_frequent(vec![1, 1, 1, 2, 2, 3], 2),
+            vec![1, 2]
+        );
+        assert_eq!(Solution::top_k_frequent(vec![1], 1), vec![1]);
+    }
+}
